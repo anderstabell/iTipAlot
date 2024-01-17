376fcd82404eab69a02f2be00049a5044badf88c
Anders Tabell <100650184+anderstabell@users.noreply.github.com>
2024-01-17 08:41:25 -0500

    Initial Commit

---
+++ iTipAlot/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iTipAlot
+//
+//  Created by Anders Tabell on 1/17/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iTipAlot/iTipAlotApp.swift
@@ -0,0 +1,17 @@
+//
+//  iTipAlotApp.swift
+//  iTipAlot
+//
+//  Created by Anders Tabell on 1/17/24.
+//
+
+import SwiftUI
+
+@main
+struct iTipAlotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
