fe96f1043b6330619cb3556f54d989fe70cc2194
Anders Tabell <anders.tabell@pianometalstudios.com>
2024-12-19 16:58:57 -0500

    Update MainViewModel.swift

--- iTipAlot/MainViewModel.swift
+++ iTipAlot/MainViewModel.swift
@@ -10,7 +10,8 @@ import Observation
 
 @Observable final class MainViewModel {
     
-    /// Properties for the ``CardView``
+    /// Overall calculations for the three ``CardView`` properties
+    /// Properties for calculating the check
     var checkAmount = ""
     var numberOfPeople = 2
     var tipPercentage = 2
@@ -18,23 +19,27 @@ import Observation
     /// Tip amount that can be selected when doing the calculations
     let tipPercentages = [10, 15, 20, 25, 0]
     
-    
+    /// This will check if the `checkAmount` can be a `Double`. If not, make it a zero (0)
     var subTotal: Double { Double(checkAmount) ?? 0 }
     
+    /// Calculation for the subtotal per person
     var subTotalPerPerson: Double {
         let peopleCount = Double(numberOfPeople)
         let subTotal = Double(checkAmount) ?? 0
         return subTotal / peopleCount
     }
     
+    /// Calculation for the tip value
     var tipValue: Double {
         let tipSelection = Double (tipPercentages[tipPercentage])
         let subTotal = Double(checkAmount) ?? 0
         return subTotal / 100 * tipSelection
     }
     
+    /// Calculation for tip value per person
     var tipValuePerPerson: Double { tipValue / Double(numberOfPeople) }
     
+    /// Calculation for total amount, tip included
     var totalAmountWithTip: Double {
         let tipSelection = Double (tipPercentages[tipPercentage])
         let subTotal = Double(checkAmount) ?? 0
@@ -43,6 +48,7 @@ import Observation
         return grandTotal
     }
     
+    /// Calculation for the grand total per person
     var totalPerPerson: Double {
         let peopleCount = Double(numberOfPeople)
         let amountPerPerson = totalAmountWithTip / peopleCount
