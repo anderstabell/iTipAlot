e5bb66cf9f336901a3c73d30092c4bf8caaf315a
Anders Tabell <anders.tabell@pianometalstudios.com>
2025-01-20 11:50:27 -0500

    Update MainView.swift

--- iTipAlot/MainView.swift
+++ iTipAlot/MainView.swift
@@ -42,7 +42,6 @@ struct MainView: View {
                 GuestCountView(guestCount: $vm.numberOfPeople)
             }
             .padding()
-            .navigationTitle("We like to tip!")
             .background(Image("dollar").opacity(0.2))
         }
     }
