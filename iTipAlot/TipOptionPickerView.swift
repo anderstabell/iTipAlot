b10a4322f129ab5f360d6e3423c910dd0066a1ef
Anders Tabell <anders.tabell@pianometalstudios.com>
2025-01-23 14:51:31 -0500

    Added `TipOptionPickerView`

---
+++ iTipAlot/TipOptionPickerView.swift
@@ -0,0 +1,25 @@
+//
+//  TipOptionPickerView.swift
+//  iTipAlot
+//
+//  Created by Anders Tabell on 1/23/25.
+//
+
+import SwiftUI
+
+struct TipOptionPickerView: View {
+    
+    @Binding var tipOption: TipOption
+    
+    var body: some View {
+        Picker("Tip Option", selection: $tipOption) {
+            Text("Percentage").tag(TipOption.percentage)
+            Text("Custom $ Tip").tag(TipOption.customAmount)
+        }
+        .pickerStyle(.segmented)
+    }
+}
+
+#Preview {
+    TipOptionPickerView(tipOption: .constant(.percentage))
+}
