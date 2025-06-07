e4c61c8dc31fb37cae6643d9e603d6c668113906
Anders Tabell <anders.tabell@pianometalstudios.com>
2025-06-07 09:10:48 -0400

    Moved `TipOption` to its own file

--- iTipAlot/MainViewModel.swift
+++ iTipAlot/MainViewModel.swift
@@ -11,15 +11,6 @@ import Observation
 /// Represents the two ways a tip can be specified: as a percentage of the subtotal
 /// or as a fixed custom dollar amount.
 ///
-/// This enum is used by ``MainViewModel/tipOption`` to determine the calculation logic for the tip.
-enum TipOption {
-    /// Tip is calculated as a percentage of the `checkAmount`.
-    /// The percentage value is stored in ``MainViewModel/tipPercentage``.
-    case percentage
-    /// Tip is a fixed dollar amount specified by ``MainViewModel/customTipAmount``.
-    case customAmount
-}
-
 /// This class manages the state and logic for calculating tips, designed to be observed by SwiftUI views.
 ///
 /// It holds all input values like check amount, number of people, and tip preferences,
